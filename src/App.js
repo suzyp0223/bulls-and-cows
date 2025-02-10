608c01a64190bc376530e4d2b8e24b6e52b6e29a
suzy <suzyp0223@gmail.com>
2025-02-10 23:10:20 +0900

    로직구현

--- src/App.js
+++ src/App.js
@@ -14,13 +14,46 @@ function App() {
     setAnswer(event.target.value);
   };
 
+  const handleSubmit = () => {
+    // 스트라이크, 볼, 정답 유무
+
+    const answers = answer.split("").map((item) => Number(item));
+
+    const { strike, ball } = randomNo.reduce(
+      (prev, cur, idx) => {
+        // 같은 자리에 같은 수가 존재하면 스트라이크
+        if (answers[idx] === cur) {
+          return {
+            ...prev,
+            strike: prev.strike + 1,
+          };
+        }
+        // 다른 자리에 수가 존재하면 볼
+        if (answers.includes(cur)) {
+          return {
+            ...prev,
+            ball: prev.ball + 1,
+          };
+        }
+
+        return prev;
+      },
+      {
+        strike: 0,
+        ball: 0,
+      }
+    );
+
+    console.log("strike", strike, "ball", ball);
+  };
+
   return (
     <div className="App">
       <h1>숫자 야구 게임</h1>
       <header className="header">{randomNo}</header>
       <section>
         <input type="text" value={answer} onChange={handleAnswerChange} />
-        <button>맞춰보기</button>
+        <button onClick={handleSubmit}>맞춰보기</button>
       </section>
       <h2>기록</h2>
       <ol>
