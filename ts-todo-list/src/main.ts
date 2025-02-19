d162d61a58ce35f9a8b293d14d6eae36a5bf66d9
suzy <suzyp0223@gmail.com>
2025-02-19 18:39:32 +0900

    타입추가

--- ts-todo-list/src/main.ts
+++ ts-todo-list/src/main.ts
@@ -19,7 +19,10 @@ class TodoApp {
   initEvent() {
     const inputEl = document.querySelector("#todo-input");
 
-    inputEl.addEventListener("keydown", this.addTodo.bind(this));
+    // 타입가드
+    if (inputEl) {
+      inputEl?.addEventListener("keydown", this.addTodo.bind(this));
+    }
   }
 
   /**
@@ -27,12 +30,17 @@ class TodoApp {
    * 엔터로 동작 > event등록
    * @param {string} text
    */
-  addTodo(event) {
+  addTodo(event: KeyboardEventInit) {
     if (event.key !== "Enter") {
       return;
     }
 
-    const target = event.target;
+    // as 어설션은 타입 추론을 덮어쓰고 명시적으로 타입을 선언하는 방식
+    //  컴파일러에게 "내가 이 타입을 확신한다"라고 말하는 것에 가까움.
+    // // <>는 타입스크립트에서  타입 어설션을 제네릭 형태로 사용하는 방법.
+    // // event가 KeyboardEvent임을 먼저 어설션하고, 그 안에서
+    // // **target**이 실제로 HTMLInputElement라는 타입임을 다시 한 번 어설션
+    const target = <HTMLInputElement>(event as KeyboardEvent).target;
 
     // 입력값 없을시
     if (!target.value) {
@@ -45,7 +53,7 @@ class TodoApp {
       content: target.value,
     });
 
-    target.value = '';
+    target.value = "";
     this.render(this.todoList);
   }
 
@@ -73,7 +81,7 @@ class TodoApp {
    * @param {string} filterType
    * @returns {Todo[]} 필터링된 할일
    */
-  getTodoListByFilter(filterType) {}
+  // getTodoListByFilter(filterType) {}
 
   /**
    * 할 일의 내용과 상태를 수정할 수 있다.
@@ -82,21 +90,21 @@ class TodoApp {
    * @param {string} [todo.text]  -수정될 내용
    * @param {string} [todo.status]  -수정될 상태
    */
-  updateTodo({ id, text, status }) {}
+  // updateTodo({ id, text, status }) {}
 
   /**
    * 특정 할 일을 제거할 수 있다.
    *
    * @param {number} id
    */
-  removeTodo(id) {}
+  // removeTodo(id) {}
 
-  generateTodoList(todoList) {
+  generateTodoList(todo: Todo) {
     const containerEl = document.createElement("div");
     const todoTemplate = `<div class="item__div">
-    <input type='checkbox' ${todoList.isDone && "checked"} />
-    <div class='content ${todoList.isDone && "checked"}' contentEditable>${
-      todoList.content
+    <input type='checkbox' ${todo.isDone && "checked"} />
+    <div class='content ${todo.isDone && "checked"}' contentEditable>${
+      todo.content
     }</div>
     <button>X</button>
     </div>`;
@@ -107,7 +115,8 @@ class TodoApp {
     return containerEl;
   }
 
-  render(todoList = []) {
+  // Todo[] = [] 호출 시 인자가 없으면 빈 배열이 자동으로 전달.
+  render(todoList: Todo[] = []) {
     const todoListEl = document.querySelector(".todo-items");
 
     // 가상의 돔. 실질적으로 그려지지않은 상태
@@ -117,7 +126,12 @@ class TodoApp {
     );
 
     fragment.append(...todoListComponent);
-    todoListEl.appendChild(fragment);
+    // todoListEl?.appendChild(fragment);  //?를 넣으면 ?앞 값이 존재할때만 동작 아래 타입가드와 동일함.
+
+    // 타입가드 - null일것 같을때
+    if (todoListEl) {
+      todoListEl.appendChild(fragment);
+    }
   }
 }
 
